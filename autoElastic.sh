2f6dd9fa87739fc0586f78985f49241b591b8556
Karl Hubbard <potatrix@gmail.com>
2020-12-27 17:41:12 -0800

    still a sudo left to get rid of

--- autoElastic.sh
+++ autoElastic.sh
@@ -4,7 +4,7 @@
 echo "downloading Elasticsearch"
 apt-get install apt-transport-https
 
-echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee /etc/apt/sources.list.d/elastic-7.x.list
 
 apt-get update
 
