ddc9fa55e2f0fddd62867fce2dc7737025f5202f
Karl Hubbard <potatrix@gmail.com>
2020-12-27 17:36:00 -0800

    remove sudo from certain lines

--- autoElastic.sh
+++ autoElastic.sh
@@ -6,13 +6,13 @@ apt-get install apt-transport-https
 
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install elasticsearch kibana filebeat metricbeat
+apt-get install elasticsearch kibana filebeat metricbeat
 
 echo "enabling Elasticsearch, kibana, filebeat, and metricbeat"
-sudo systemctl enable elasticsearch kibana filebeat metricbeat
+systemctl enable elasticsearch kibana filebeat metricbeat
 
 echo "running metricbeat and filebeat setup"
-sudo metricbeat setup
-sudo filebeat setup
+metricbeat setup
+filebeat setup
